9449de632142ec7135b3f94e2e13abc01d3df252
José Augusto <joseneto.design@gmail.com>
2015-08-17 11:38:45 -0300

    Falha no Npm Link

--- src/actionbar.js
+++ src/actionbar.js
@@ -20,19 +20,19 @@ var HActionbar = React.createClass({
         render_tertiary()
         render_tertiary()
         render_tertiary()
-        while (action_names.length > children.length + (dropdownMenuItems?dropdownMenuItems.length:0))
+        while (action_names.length > children.length + (dropdownMenuItems?Object.keys(dropdownMenuItems).length:0))
             render_menu_item();
         if (dropdownMenuItems)
             createDropdownMenu();
-        children.unshift(React.createElement("button", {style: {position: 'fixed', top: 0, right: 0},onClick: function(){_this.refs.menu.toggleDropDown()}}, 'menu'));
-        return React.createElement("div", {className: 'h_actionbar'}, children);
+//        children.unshift(React.createElement("button", {style: {position: 'fixed', top: 0, right: 0},onClick: function(){_this.refs.menu.toggleDropDown()}}, 'menu'));
+        return React.createElement("div", {key: "ActionBar", className: 'h_actionbar', children: children});
 
         function render_primary()
         {
             if (children.length>=action_names.length)
                 return;
             var action_name = action_names[children.length];
-            var child = React.createElement(H5Action, {store: store, action: action_name, className: 'position_right'});
+            var child = React.createElement(H5Action, {store: store, action: action_name, key: "actionBarPrimary_" + action_name, run: function(){}, className: 'position_right'});
             children.unshift(child);
         }
 
@@ -41,7 +41,7 @@ var HActionbar = React.createClass({
             if (children.length>=action_names.length)
                 return;
             var action_name = action_names[children.length];
-            var child = React.createElement(H5Action, {store: store, action: action_name, className: 'position_right'});
+            var child = React.createElement(H5Action, {store: store, action: action_name, key: "actionBarSecondary_" + action_name, run: function(){}, className: 'position_right'});
             children.unshift(child);
         }
 
@@ -50,18 +50,18 @@ var HActionbar = React.createClass({
             if (children.length>=action_names.length)
                 return;
             var action_name = action_names[children.length];
-            var child = React.createElement(H5Action, {store: store, action: action_name, className: 'position_left'});
+            var child = React.createElement(H5Action, {store: store, key: "actionBarTertiary_" + action_name, action: action_name, run: function(){}, className: 'position_left'});
             children.unshift(child);
         }
 
         function render_menu_item()
         {
-            if (children.length + (dropdownMenuItems?dropdownMenuItems.length:0) >= action_names.length)
+            if (children.length + (dropdownMenuItems?Object.keys(dropdownMenuItems).length:0) >= action_names.length)
                 return;
             if (!dropdownMenuItems)
-               dropdownMenuItems = []
-            var action_name = action_names[children.length + dropdownMenuItems.length];
-            dropdownMenuItems.unshift(actions[action_name]);
+               dropdownMenuItems = {}
+            var action_name = action_names[children.length + Object.keys(dropdownMenuItems).length];
+            dropdownMenuItems[action_name] = actions[action_name];
         }
 
         function createDropdownMenu()
@@ -71,6 +71,7 @@ var HActionbar = React.createClass({
 //               propsMenuDropdown.top = this.props.top;
 //               propsMenuDropdown.bottom = this.props.bottom;
                var chield = React.createElement(H5MenuDropdown, {
+                   key: "MenuDropDrow",
                    ref: 'menu',
                    actions: dropdownMenuItems,
                    icon: _this.props.icon + " position_icon_actionbar"
--- test/template/app/actionbar.view.js
+++ test/template/app/actionbar.view.js
@@ -5,11 +5,106 @@ window.hsession = {
     language: 'pt_br'
 };
 
+//var mock_store = {
+// actions: {
+////     ___menudropdown___,
+//     ___mock___
+//  }
+//};
+
 var mock_store = {
  actions: {
-     ___menudropdown___,
-     ___mock___
-  }
+        voltar: {
+            labelText: 'Voltar',
+            mode: 'visible',
+            kind: 'secondary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Sair da tela de edição do paciente'
+        },
+        tirar_foto: {
+            labelText: 'Tirar Foto',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_1: {
+            labelText: 'Tirar Foto 1',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_2: {
+            labelText: 'Tirar Foto 2',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_3: {
+            labelText: 'Tirar Foto 3',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_4: {
+            labelText: 'Tirar Foto 4',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_5: {
+            labelText: 'Tirar Foto 5',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_6: {
+            labelText: 'Tirar Foto 6',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_7: {
+            labelText: 'Tirar Foto 7',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_8: {
+            labelText: 'Tirar Foto 8',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        }
+    }
 };
 
 function salva_info(){
