eaaae51b6f63c72df149794e64fd9cdcd83fd736
MarcelloVSilva <mvnota10@gmail.com>
2015-08-24 11:58:20 -0300

    action

--- test/template/app/actionbar.view.js
+++ test/template/app/actionbar.view.js
@@ -5,119 +5,118 @@ window.hsession = {
     language: 'pt_br'
 };
 
-//var mock_store = {
-// actions: {
-////     ___menudropdown___,
-//     ___mock___
-//  }
-//};
-
 var mock_store = {
- actions: {
-        voltar: {
-            labelText: 'Voltar',
-            mode: 'visible',
-            kind: 'primary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Sair da tela de edição do paciente'
-        },
-        tirar_foto: {
-            labelText: 'Tirar Foto',
-            mode: 'visible',
-            kind: 'tertiary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Tirar foto do paciente'
-        },
-        tirar_foto_1: {
-            labelText: 'Tirar Foto 1',
-            mode: 'visible',
-            kind: 'tertiary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Tirar foto do paciente'
-        },
-        tirar_foto_2: {
-            labelText: 'Tirar Foto 2',
-            mode: 'visible',
-            kind: 'tertiary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Tirar foto do paciente'
-        },
-        tirar_foto_3: {
-            labelText: 'Tirar Foto 3',
-            mode: 'visible',
-            kind: 'tertiary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Tirar foto do paciente'
-        },
-        tirar_foto_4: {
-            labelText: 'Tirar Foto 4',
-            mode: 'visible',
-            kind: 'tertiary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Tirar foto do paciente'
-        },
-        tirar_foto_5: {
-            labelText: 'Tirar Foto 5',
-            mode: 'visible',
-            kind: 'tertiary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Tirar foto do paciente'
-        },
-        tirar_foto_6: {
-            labelText: 'Tirar Foto 6',
-            mode: 'visible',
-            kind: 'tertiary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Tirar foto do paciente'
-        },
-        tirar_foto_7: {
-            labelText: 'Tirar Foto 7',
-            mode: 'visible',
-            kind: 'tertiary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Tirar foto do paciente'
-        },
-        tirar_foto_8: {
-            labelText: 'Tirar Foto 8',
-            mode: 'visible',
-            kind: 'tertiary',
-            run: function () {
-                alert("OK");
-            },
-            hintText: 'Tirar foto do paciente'
-        }
+    actions: {
+        ___mock___
+        ___menudropdown___
     }
 };
+//var mock_store = {
+// actions: {
+//        voltar: {
+//            labelText: 'Voltar',
+//            mode: 'visible',
+//            kind: 'primary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Sair da tela de edição do paciente'
+//        },
+//        tirar_foto: {
+//            labelText: 'Tirar Foto',
+//            mode: 'visible',
+//            kind: 'tertiary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Tirar foto do paciente'
+//        },
+//        tirar_foto_1: {
+//            labelText: 'Tirar Foto 1',
+//            mode: 'visible',
+//            kind: 'tertiary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Tirar foto do paciente'
+//        },
+//        tirar_foto_2: {
+//            labelText: 'Tirar Foto 2',
+//            mode: 'visible',
+//            kind: 'tertiary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Tirar foto do paciente'
+//        },
+//        tirar_foto_3: {
+//            labelText: 'Tirar Foto 3',
+//            mode: 'visible',
+//            kind: 'tertiary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Tirar foto do paciente'
+//        },
+//        tirar_foto_4: {
+//            labelText: 'Tirar Foto 4',
+//            mode: 'visible',
+//            kind: 'tertiary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Tirar foto do paciente'
+//        },
+//        tirar_foto_5: {
+//            labelText: 'Tirar Foto 5',
+//            mode: 'visible',
+//            kind: 'tertiary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Tirar foto do paciente'
+//        },
+//        tirar_foto_6: {
+//            labelText: 'Tirar Foto 6',
+//            mode: 'visible',
+//            kind: 'tertiary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Tirar foto do paciente'
+//        },
+//        tirar_foto_7: {
+//            labelText: 'Tirar Foto 7',
+//            mode: 'visible',
+//            kind: 'tertiary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Tirar foto do paciente'
+//        },
+//        tirar_foto_8: {
+//            labelText: 'Tirar Foto 8',
+//            mode: 'visible',
+//            kind: 'tertiary',
+//            run: function () {
+//                alert("OK");
+//            },
+//            hintText: 'Tirar foto do paciente'
+//        }
+//    }
+//};
 
-function salva_info(){
+function salva_info() {
 
- mock_store.actions.salvar.labelText = 'Salvo'
+    mock_store.actions.salvar.labelText = 'Salvo'
 
 };
 
-function pega_dados_troca_nome(){
+function pega_dados_troca_nome() {
 
- var nome = document.getElementsByTagName('input');
- var nome_btn = nome[0].value;
- mock_store.actions.muda_nome.labelText = nome_btn;
+    var nome = document.getElementsByTagName('input');
+    var nome_btn = nome[0].value;
+    mock_store.actions.muda_nome.labelText = nome_btn;
 
 };
 
@@ -133,4 +132,4 @@ var AppAction = React.createClass({
 
 
 
-React.render(<AppAction/>, document.getElementById('app'));
+React.render(React.createElement(AppAction), document.getElementById('app'));
--- test/template/test/teste_inicial.spec
+++ test/template/test/teste_inicial.spec
@@ -1,6 +1,7 @@
 @objects
    div   id  app 
    actionbar  css .h_actionbar
+   icon_dropdown  css  .fa-ellipsis-v
    btn_primary  css  .h_actionbar button.salvar
    btn_secondary  css .h_actionbar button.voltar
    btn_tertiary   css .h_actionbar button.tirar_foto 
