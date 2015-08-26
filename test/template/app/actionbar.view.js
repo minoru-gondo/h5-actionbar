4fa9d34e2c940d3144e4c4fe71cebbd19cb2e7f0
thr0w <josefernandotolentino@gmail.com>
2015-08-26 07:16:52 -0300

    retirada de função não utilizada

--- test/template/app/actionbar.view.js
+++ test/template/app/actionbar.view.js
@@ -111,14 +111,6 @@ function salva_info() {
 
 };
 
-function pega_dados_troca_nome() {
-
-    var nome = document.getElementsByTagName('input');
-    var nome_btn = nome[0].value;
-    mock_store.actions.muda_nome.labelText = nome_btn;
-
-};
-
 var AppAction = React.createClass({
     render: function () {
         return React.createElement(H5ActionBar, {
